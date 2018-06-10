89121dee483d5d4ee5dcb46acb5240605831ef48
Andriy Mykhajlyshyn <amykh2007@gmail.com>
2018-06-10 21:16:36 +0300

    1.12.4.13-vrenetic

--- recipes/custom.py
+++ recipes/custom.py
@@ -13,7 +13,7 @@ from cerbero.utils import shell, to_unixpath
 class GStreamerBase:
 
     licenses = [License.LGPLv2Plus]
-    version = '1.12.4.1'
+    version = '1.12.4.13'
     commit = 'origin/1.12'
 
 class GStreamer(GStreamerBase, recipe.Recipe):
